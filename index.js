b6533e6070081f3f2995a0a7ef9a08c769162e20
Jalal Al-Madhaji <jalal.almadhaji90@gmail.com>
2019-10-09 09:51:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = 4;
+
+let max = Math.max((10,3,5,7,20));
