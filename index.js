11beb1fada6f2d22acb896dc1435de1a1382c6c7
Aisha Rashed <aisha1rashed@gmail.com>
2019-10-29 19:04:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
+//declare variables
+let num1= 31;
+let num2 = 2;
+let multiply = num1 * num2; //multiply 2 numbers
+let random = Math.floor(Math.random() * 10) + 1; //generate a random //integer
+let mod = 10 % 6 ; // the remainder
+let max =Math.max(1,5,10,20); //highest number = 20
 // Write your code here
