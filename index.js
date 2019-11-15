24207bf81eb177625a96b51b518b9ac7d4cd6f40
TaghreedNageeb <taghreed.n.d2018@gmail.com>
2019-11-15 10:03:00 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+let num1= 4,
+num2=15.5,
+num3=5,
+num4= 64;
+
+
+let multiply= num1 * num2;
+console.log(multiply)
+
+let random= Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let mod= num4 % num3;
+console.log(mod);
+
+let max= Math.max(8,13, 20, 9, 16);
+console.log(max);
