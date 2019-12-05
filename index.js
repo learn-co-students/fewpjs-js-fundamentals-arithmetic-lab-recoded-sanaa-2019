b779fdf1960b4d53a31056c3e2c1716f3117057c
Sarah Alafif <sar1997yah@gmail.com>
2019-12-05 21:48:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ let num2=2
 let multiply=num1*num2
 let random=Math.floor(Math.random())+1
 let mod=(9%2)+3
-max=Math.max(1, 2, 3, 4, 5,20);
+let max=Math.max(1, 2, 3, 4, 5,20);
 // Write your code here
